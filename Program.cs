754b27f1fc9d5b40ee02b239641d9e99970ba120
IrinaBel-84 <123074826+IrinaBel-84@users.noreply.github.com>
2023-03-15 00:10:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+string[] array = new string[] {"hello","world", "7", "5215341", "123", "234", "00", "1", "2", "3", "4"};
+for (int i=0; i < array.Length; i++)
+{ 
+    if (array[i].Length <=3)
+    Console.Write(array[i] + " ");
+}
